ab64939883215e9cb14845d8fb3fe09c1b0fdc4e
DoLim <limdongyoung0@gmail.com>
2020-04-10 08:50:22 +0000

    Made frame of query_resolver

--- handle/query_resolver.go
+++ handle/query_resolver.go
@@ -14,6 +14,31 @@ func (*RootResolver) TWL(args TWLArgs) TWLResolver {
 }
 
 // Authors query resolver
-func (*RootResolver) Authors() {
+func (*RootResolver) Authors(args AuthorsArgs) []*AuthorResolver {
+	return nil
+}
 
+// Author query resolver
+func (*RootResolver) Author(args AuthorArgs) AuthorResolver {
+	return AuthorResolver{}
+}
+
+// Groups query resolver
+func (*RootResolver) Groups(args GroupsArgs) []*GroupResolver {
+	return nil
+}
+
+// Group query resolver
+func (*RootResolver) Group(args GroupArgs) GroupResolver {
+	return GroupResolver{}
+}
+
+// Contents query resolver
+func (*RootResolver) Contents(args ContentsArgs) []*ContentResolver {
+	return nil
+}
+
+// Content query resolver
+func (*RootResolver) Content(args ContentArgs) ContentResolver {
+	return ContentResolver{}
 }
