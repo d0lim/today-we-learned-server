7440f54fdeb9723f73ed3a213500c7f6043fd093
DoLim <limdongyoung0@gmail.com>
2020-04-19 08:22:42 +0000

    Added vscode setting and prettier and dummy data

---
+++ backend-node/graphql/dummy.js
@@ -0,0 +1,43 @@
+// Groups is dummy data of Group
+const Groups = {
+  "0": {
+    ID: "0",
+    Title: "Study",
+    Description: "Group for Studying",
+    UserID: ["0", "1", "2"],
+  },
+  "1": {
+    ID: "1",
+    Title: "Exercise",
+    Description: "Group for Exercise",
+    UserID: ["0", "1"],
+  },
+  "2": {
+    ID: "2",
+    Title: "Coffee",
+    Description: "Group for making Coffee",
+    UserID: ["0", "2"],
+  },
+};
+
+// Users is dummy data of Author
+var Users = {
+  "0": {
+    ID: "0",
+    Name: "DoLim",
+    ProfileImage: "https://api.adorable.io/avatars/285/dolim",
+    GroupID: ["0", "1", "2"],
+  },
+  "1": {
+    ID: "1",
+    Name: "Collin",
+    ProfileImage: "https://api.adorable.io/avatars/285/collin",
+    GroupID: ["0", "1"],
+  },
+  "2": {
+    ID: "2",
+    Name: "John",
+    ProfileImage: "https://api.adorable.io/avatars/285/john",
+    GroupID: ["0", "2"],
+  },
+};
