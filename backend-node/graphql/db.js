bf2654df272ccd55e2ee30bedb921582dc6c4ffc
DoLim <limdongyoung0@gmail.com>
2020-04-20 14:46:43 +0000

    Writing select Query

--- backend-node/graphql/db.js
+++ backend-node/graphql/db.js
@@ -1,4 +1,7 @@
-import { Groups, Users, Posts, Activities } from "./dummy";
+import Activity from "../models/Activity";
+import User from "../models/User";
+import Group from "../models/Group";
+import Post from "../models/Post";
 import mongoose from "mongoose";
 
 export const connectToMongoDB = async (password) => {
@@ -9,20 +12,29 @@ export const connectToMongoDB = async (password) => {
   });
 };
 
-export const getUsers = (groupId) =>
-  Users.filter((user) => user.groupId.includes(groupId));
-
-export const getUser = (userId) => Users.find((user) => user.id === userId);
-
-export const getGroups = (title) => {
-  console.log("title is ", title);
-  if (title !== undefined)
-    return Groups.filter((group) => group.title.includes(title));
-  return Groups;
+export const getUsers = async (_groupId) => {
+  return await User.find({ groupId: { $elemMatch: _groupId } });
 };
 
-export const getGroup = (groupId) =>
-  Groups.find((group) => group.id === groupId);
+export const getUser = async (_userId) => {
+  return await User.findById(_userId);
+  // Users.find((user) => user.id === userId);
+};
+
+export const getGroups = async (_title) => {
+  // console.log("title is ", title);
+  // if (title !== undefined)
+  //   return Groups.filter((group) => group.title.includes(title));
+  // return Groups;
+  if (_title !== undefined)
+    return await Group.find({ title: { $regex: `.*${_title}.*` } });
+  return await Group.find();
+};
+
+export const getGroup = async (_groupId) => {
+  // Groups.find((group) => group.id === groupId);
+  return await Group.findById(_groupId);
+};
 
 export const getPosts = (groupId) =>
   Posts.filter((post) => post.groupId === groupId);
